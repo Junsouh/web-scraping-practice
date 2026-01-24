5482819a8a53fd1889462946d4c744b50b2a3732
Jun Hong <junsouh@gmail.com>
2026-01-24 11:18:01 -0500

    feat: nodriver scraper with analysis of data

---
+++ analysis.py
@@ -0,0 +1,69 @@
+import os
+import glob
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def analyze_html(folder_path):
+    data = []
+
+    files = glob.glob(os.path.join(folder_path, "*.html"))
+
+    for file in files:
+        with open(file, "r", encoding="utf-8") as f:
+            soup = BeautifulSoup(f.read(), "html.parser")
+
+            for script_style in soup(["script", "style"]):
+                script_style.decompose()
+            text = soup.get_text().lower()
+
+            # Uncomment to print the resulting text
+            # print(f"Text for {file}: {text}\n")
+
+            scores = {
+                "Product/Asset": text.count("photo") + text.count("download") + text.count("template"),
+                "Legal/Info": text.count("privacy") + text.count("legal") + text.count("terms") + text.count("data"),
+            }
+
+            assigned_category = max(scores, key=scores.get)
+            if scores[assigned_category] < 10:
+                assigned_category = "General/Other"
+
+            data.append({
+                "Filename": os.path.basename(file),
+                "Category": assigned_category,
+                "Text_Length": len(text)
+            })
+
+    df = pd.DataFrame(data)
+    df.to_csv("scraping_analysis.csv", index=False)
+    return df
+
+def scraping_stats(csv):
+    df = pd.read_csv(csv)
+
+    category_counts = df['Category'].value_counts()
+
+    print("--- Text Statistics ---")
+    # Average lengths
+    avg_text = df.groupby('Category')['Text_Length'].mean().round(2)
+    
+    # Percentage of each category
+    percentages = (df['Category'].value_counts(normalize=True) * 100).round(2)
+
+    # Combine into df
+    stats_df = pd.DataFrame({
+        'Total Pages': category_counts,
+        'Percentage (%)': percentages,
+        'Avg Text Length (chars)': avg_text
+    })
+    
+    print(stats_df)
+
+def main():
+    data_folder = "html_files/"
+    df = analyze_html(data_folder)
+    
+    scraping_stats("scraping_analysis.csv")
+
+if __name__ == "__main__":
+    main()
---
+++ research_scraper.py
@@ -0,0 +1,84 @@
+import nodriver as uc
+import asyncio
+import os
+from collections import deque
+import shutil
+
+SCRAPING_WEBSITE = "https://editablepsd.xyz/"
+
+async def save_page(page, url, folder):
+    try:
+        content = await page.get_content()
+        filename = url.replace("https://", "").replace("http://", "").replace("/", "_").rstrip("_") + ".html"
+
+        path = os.path.join(folder, filename)
+
+        with open(path, "w", encoding="utf-8") as f:
+            f.write(content)
+        print(f"Successfully saved {url}")
+
+    except Exception as e:
+        print(f"Failed to save {url}: {e}")
+
+async def main():
+    # Start the browser
+    browser = await uc.start(headless=True)
+    
+    # Delete old folder for data
+    data_folder = "html_files/"
+    if os.path.exists(data_folder):
+        print(f"Cleaning old data from data folder\n")
+        shutil.rmtree(data_folder)
+
+    # Create new folder for data
+    os.makedirs(data_folder)
+
+    # Create queue and set for BFS
+    links_queue = deque([SCRAPING_WEBSITE])
+    links_found = {SCRAPING_WEBSITE}
+    SEARCH_DEPTH = 10
+
+    # Begin BFS
+    current_depth = 0
+    links_count = 0
+    while (len(links_queue) > 0 and current_depth < SEARCH_DEPTH):
+        links_count = len(links_found)
+        print(f"\n--- Scraping Depth {current_depth} ---")
+        
+        link_count = len(links_queue)
+
+        for _ in range(link_count):
+            current_url = links_queue.popleft()
+
+            print(f"Visiting: {current_url}")
+            page = await browser.get(current_url)
+
+            await page.sleep(10)
+            await save_page(page, current_url, data_folder)
+
+            all_tags = await page.select_all('a[href]')
+            for tag in all_tags:
+                href = tag['href']
+
+                if not href: 
+                    continue
+
+                full_link = ""
+                if href.startswith('/'):
+                    full_link = SCRAPING_WEBSITE.rstrip('/') + href
+                elif SCRAPING_WEBSITE in href:
+                    full_link = href
+                
+                if full_link and (full_link not in links_found):
+                    links_found.add(full_link)
+                    links_queue.append(full_link)
+
+
+        current_depth += 1
+
+    print(f"\n Done, found and saved {links_count} pages")
+    print(f"{len(links_found) - links_count} left to scrape")
+
+if __name__ == '__main__':
+    # This is how you run an 'async' function in nodriver
+    uc.loop().run_until_complete(main())
